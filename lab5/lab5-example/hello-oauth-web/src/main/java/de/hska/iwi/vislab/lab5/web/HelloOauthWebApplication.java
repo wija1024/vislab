63b95df6da1b2e418dc1123e6d5a2fd53f2ae0f6
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2017-05-23 22:36:49 +0200

    add lab5

---
+++ lab5/lab5-example/hello-oauth-web/src/main/java/de/hska/iwi/vislab/lab5/web/HelloOauthWebApplication.java
@@ -0,0 +1,66 @@
+package de.hska.iwi.vislab.lab5.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Client
+@Controller
+public class HelloOauthWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOauthWebApplication.class, args);
+	}
+
+	@Value("${oauth.resource:http://localhost:8080}")
+	private String baseUrl;
+
+	@Value("${oauth.authorize:http://localhost:8080/oauth/authorize}")
+	private String authorizeUrl;
+
+	@Value("${oauth.token:http://localhost:8080/oauth/token}")
+	private String tokenUrl;
+
+	@Autowired
+	private OAuth2RestOperations restTemplate;
+
+  @RequestMapping("/")
+  public String home() {
+  	return "greet";
+  }
+
+  @RequestMapping("/greet")
+	public String next(Model model) {
+		String hello = restTemplate.getForObject(baseUrl + "/hello", String.class);
+		model.addAttribute("greet", hello);
+		return "greet";
+	}
+
+	@Bean
+	public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
+		return new OAuth2RestTemplate(resource(), oauth2ClientContext);
+	}
+
+	@Bean
+	protected OAuth2ProtectedResourceDetails resource() {
+		AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+		resource.setAccessTokenUri(tokenUrl);
+		resource.setUserAuthorizationUri(authorizeUrl);
+		resource.setClientId("my-trusted-client");
+		return resource ;
+	}
+
+}
